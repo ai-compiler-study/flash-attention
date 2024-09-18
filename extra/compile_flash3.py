1bbc01b5b6808525f1411191b533008d2ff12552
antonio <47569261+antferdom@users.noreply.github.com>
2024-09-18 15:19:19 +0000

    (ADD): print torch aten operators and compile flash_attn_func

--- extra/compile_flash3.py
+++ extra/compile_flash3.py
@@ -1,4 +1,6 @@
 from flash3 import hopper_mha
+from flash_attn_interface import flash_attn_func
+
 
 import torch
 
@@ -7,4 +9,11 @@ fn = torch.compile(
     fullgraph=True,
     backend="inductor",
     )
-print("compilation done")
+print("compilation of HopperMHA done")
+
+flash_v3_fn = torch.compile(
+    flash_attn_func,
+    fullgraph=True,
+    backend="inductor",
+    )
+print(f"compilation of {flash_v3_fn.__name__} done")
